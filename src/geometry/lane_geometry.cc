57d43e2b5c6a38ebe0b84f09c3098ff581b6c25c
Franco Cipollone <franco.c@ekumenlabs.com>
2022-08-16 13:58:02 -0300

    Adds LaneGeometry class.
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>

---
+++ src/geometry/lane_geometry.cc
@@ -0,0 +1,112 @@
+// BSD 3-Clause License
+//
+// Copyright (c) 2022, Woven Planet.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include <memory>
+
+#include <maliput/common/maliput_copyable.h>
+#include <maliput/math/roll_pitch_yaw.h>
+#include <maliput/math/vector.h>
+
+#include "maliput/maliput_sample/geometry/line_string.h"
+
+namespace maliput_sample {
+namespace geometry {
+
+namespace {
+
+// TODO(): Move this method to a common place for all geometry helper methods.
+//
+// Returns the piecewise linearly interpolated point at the given distance.
+// @param lineString the lineString to iterate. Size must be >0.
+// @param p distance along linestring.
+// @return The interpolated point (a new point if not perfectly matching)
+//
+maliput::math::Vector3 InterpolatedPointAtP(const LineString<maliput::math::Vector3>& line_string, double p) {
+  MALIPUT_THROW_MESSAGE("Not implemented yet.");
+}
+
+maliput::math::Vector3 GetPHat(const LineString<maliput::math::Vector3>& line_string, double p) {
+  MALIPUT_THROW_MESSAGE("Not implemented yet.");
+}
+
+}  // namespace
+
+LaneGeometry::LaneGeometry(const LineString<maliput::math::Vector3>& left,
+                           const LineString<maliput::math::Vector3>& right, double scale_length,
+                           double linear_tolerance)
+    : left_(left), right_(right), scale_length_(scale_length), linear_tolerance_(linear_tolerance) {
+  centerline_ = ComputeCenterline(left_, right_);
+}
+
+maliput::math::Vector3 LaneGeometry::W(const maliput::math::Vector3& prh) const {
+  const double p = prh.x();
+  const double r = prh.y();
+  const double h = prh.z();
+  // Obtains the point on the centerline (p, 0, 0).
+  const maliput::math::Vector3 on_centerline_point = InterpolatedPointAtP(centerline_, p);
+  // Obtains r_hat.
+  const maliput::math::Vector3 on_left_point = InterpolatedPointAtP(left_, p * left_.length() / centerline_.length());
+  const maliput::math::Vector3 on_right_point =
+      InterpolatedPointAtP(right_, p * right_.length() / centerline_.length());
+  const maliput::math::Vector3 r_direction = on_right_point - on_left_point;
+  const maliput::math::Vector3 r_hat = r_direction.normalized();
+
+  // Obtains h_hat.
+  // p_hat x r_hat = h_hat
+  // To obtain p_hat we can start from the p_hat of the boundaries and scale it to right r value.
+  const maliput::math::Vector3 left_p_hat = GetPHat(left_, p);
+  const maliput::math::Vector3 right_p_hat = GetPHat(right_, p);
+
+  const double distance_left_right = (on_right_point - on_left_point).norm();
+  const double factor = (distance_left_right / 2. + r) / distance_left_right;
+  // Gets p_hat on prh point.
+  const maliput::math::Vector3 p_hat = right_p_hat * factor + left_p_hat * (1. - factor);
+  const maliput::math::Vector3 h_hat = r_hat.cross(p_hat);
+
+  // Computes the final point
+  const maliput::math::Vector3 on_prh_point = on_centerline_point + r_hat * r + h_hat * h;
+  return on_prh_point;
+}
+
+maliput::math::Vector3 LaneGeometry::WDot(const maliput::math::Vector3& prh) const {
+  MALIPUT_THROW_MESSAGE("LaneGeometry::WDot is not implemented yet.");
+}
+
+maliput::math::RollPitchYaw Orientation(const maliput::math::Vector3& prh) const {
+  MALIPUT_THROW_MESSAGE("LaneGeometry::Orientation is not implemented yet.");
+}
+
+maliput::math::RollPitchYaw WInverse(const maliput::math::Vector3& xyz) const {
+  MALIPUT_THROW_MESSAGE("LaneGeometry::Orientation is not implemented yet.");
+}
+
+}  // namespace geometry
+}  // namespace maliput_sample
---
+++ src/geometry/lane_geometry.h
@@ -0,0 +1,98 @@
+// BSD 3-Clause License
+//
+// Copyright (c) 2022, Woven Planet.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#pragma once
+
+#include <memory>
+
+#include <maliput/common/maliput_copyable.h>
+#include <maliput/math/roll_pitch_yaw.h>
+#include <maliput/math/vector.h>
+
+#include "maliput/maliput_sample/geometry/line_string.h"
+
+namespace maliput_sample {
+namespace geometry {
+
+class LaneGeometry {
+ public:
+  MALIPUT_NO_COPY_NO_MOVE_NO_ASSIGN(LaneGeometry);
+
+  LaneGeometry(const LineString<maliput::math::Vector3>& left, const LineString<maliput::math::Vector3>& right,
+               double scale_length, double linear_tolerance);
+  ~LaneGeometry() = default;
+
+  double p0() const { return 0.; }
+  double p1() const { return centerline_.length(); }
+
+  /// @return The linear tolerance used to compute all the methods.
+  /// @see maliput::api::RoadGeometry::linear_tolerance().
+  double linear_tolerance() const { return linear_tolerance_; }
+
+  /// @return The scale length of the tolerance used to compute all the methods.
+  /// @see maliput::api::RoadGeometry::scale_length().
+  double scale_length() const { return scale_length_; }
+
+  /// Evaluates @f$ W(p, r, h) @f$.
+  ///
+  /// @param prh A vector in the LaneGeometry domain.
+  /// @return A vector in the INERTIAL Frame which is the image of the LaneGeometry.
+  maliput::math::Vector3 W(const maliput::math::Vector3& prh) const;
+
+  /// Evaluates @f$ W'(p, r, h) @f$ with respect to @f$ p @f$.
+  ///
+  /// @param prh A vector in the LaneGeometry domain.
+  /// @return The derivative of @f$ W @f$ with respect to @f$ p @f$ at @p prh.
+  maliput::math::Vector3 WDot(const maliput::math::Vector3& prh) const;
+
+  /// Evaluates the orientation in the INERTIAL Frame of the LaneGeometry at
+  /// @p prh.
+  ///
+  /// @param prh A vector in the LaneGeometry domain.
+  /// @return The orientation in the INERTIAL Frame of the LaneGeometry at @p prh.
+  maliput::math::RollPitchYaw Orientation(const maliput::math::Vector3& prh) const;
+
+  /// Evaluates @f$ W⁻¹(x, y, z) @f$.
+  ///
+  /// @param xyz A point in ℝ³ that would be used to minimize the Euclidean
+  ///        distance the image of @f$ W @f$.
+  /// @return A vector in LaneGeometry's domain whose image through @f$ W @f$ would
+  ///         minimize the Euclidean distance to @p xyz.
+  maliput::math::Vector3 WInverse(const maliput::math::Vector3& xyz) const;
+
+ private:
+  const double linear_tolerance{};
+  const double scale_length{};
+  const LineString<maliput::math::Vector3> left_{};
+  const LineString<maliput::math::Vector3> right_{};
+  LineString<maliput::math::Vector3> centerline_{};
+};
+
+}  // namespace geometry
+}  // namespace maliput_sample
