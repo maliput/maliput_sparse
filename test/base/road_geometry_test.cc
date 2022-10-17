e7667913501117cdfa0913553957a1e7ebf69d84
Franco Cipollone <53065142+francocipollone@users.noreply.github.com>
2022-10-17 17:32:45 -0300

    Overrides pendings RoadGeometry methods. (#29)
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>

--- test/base/road_geometry_test.cc
+++ test/base/road_geometry_test.cc
@@ -36,12 +36,22 @@
 #include <maliput/api/lane_data.h>
 #include <maliput/api/road_geometry.h>
 #include <maliput/math/vector.h>
+#include <maliput/test_utilities/maliput_types_compare.h>
+
+#include "maliput_sparse/builder/builder.h"
+#include "maliput_sparse/geometry/line_string.h"
 
 namespace maliput_sparse {
 namespace test {
 namespace {
 
-GTEST_TEST(RoadGeometryTest, Stub) {
+using maliput::api::InertialPosition;
+using maliput::api::LanePosition;
+using maliput::math::Vector3;
+using maliput_sparse::builder::RoadGeometryBuilder;
+using maliput_sparse::geometry::LineString3d;
+
+GTEST_TEST(RoadGeometryStubTest, Stub) {
   constexpr double kLinearTolerance{1e-12};
   constexpr double kAngularTolerance{1e-12};
   constexpr double kScaleLength{1.};
@@ -51,6 +61,89 @@ GTEST_TEST(RoadGeometryTest, Stub) {
     RoadGeometry(maliput::api::RoadGeometryId{"rg_id"}, kLinearTolerance, kAngularTolerance, kScaleLength,
                  kInertialToBackendFrameTranslation);
   });
+}
+
+class RoadGeometryTest : public ::testing::Test {
+ public:
+  void SetUp() override {
+    dut_ = builder_.StartJunction()
+               .Id(kJunctionAId)
+               .StartSegment()
+               .Id(kSegmentAId)
+               .StartLane()
+               .Id(kLaneAId)
+               .StartLaneGeometry()
+               .LeftLineString(kLeftLineStringA)
+               .RightLineString(kRightLineStringA)
+               .EndLaneGeometry()
+               .EndLane()
+               .StartLane()
+               .Id(kLaneBId)
+               .StartLaneGeometry()
+               .LeftLineString(kLeftLineStringB)
+               .RightLineString(kRightLineStringB)
+               .EndLaneGeometry()
+               .EndLane()
+               .EndSegment()
+               .EndJunction()
+               .StartBranchPoints()
+               .EndBranchPoints()
+               .Build();
+  }
+
+  static constexpr double kLinearTolerance{1e-12};
+  const maliput::api::RoadGeometryId kRoadGeometryId{"custom_rg_id"};
+  const maliput::math::Vector3 kInertialToBackendFrameTranslation{0., 0., 0.};
+  const maliput::api::JunctionId kJunctionAId{"junction_a"};
+  const maliput::api::SegmentId kSegmentAId{"segment_a"};
+  const maliput::api::LaneId kLaneAId{"lane_a"};
+  const maliput::api::LaneId kLaneBId{"lane_b"};
+  const LineString3d kLeftLineStringA{Vector3{0., 10., 0.}, Vector3{10., 10., 0.}};
+  const LineString3d kRightLineStringA{Vector3{0., 5., 0.}, Vector3{10., 5., 0.}};
+  const LineString3d kLeftLineStringB{Vector3{0., 5., 0.}, Vector3{10., 5., 0.}};
+  const LineString3d kRightLineStringB{Vector3{0., 0., 0.}, Vector3{10., 0., 0.}};
+
+  RoadGeometryBuilder builder_;
+  std::unique_ptr<maliput::api::RoadGeometry> dut_;
+};
+
+TEST_F(RoadGeometryTest, OnLaneA) {
+  const maliput::api::InertialPosition inertial_pos{5., 8., 0.};
+  auto lane = dut_->ById().GetLane(kLaneAId);
+
+  const maliput::api::RoadPositionResult expected_road_position_result{
+      {lane, LanePosition{5., 0.5, 0.}},
+      {5., 8., 0.}, /* nearest position */
+      0.,           /* distance */
+  };
+  EXPECT_TRUE(maliput::api::test::IsRoadPositionResultClose(expected_road_position_result,
+                                                            dut_->ToRoadPosition(inertial_pos), kLinearTolerance));
+}
+
+TEST_F(RoadGeometryTest, OnLaneB) {
+  const maliput::api::InertialPosition inertial_pos{5., 0., 0.};
+  auto lane = dut_->ById().GetLane(kLaneBId);
+
+  const maliput::api::RoadPositionResult expected_road_position_result{
+      {lane, LanePosition{5., -2.5, 0.}},
+      {5., 0., 0.}, /* nearest position */
+      0.,           /* distance */
+  };
+  EXPECT_TRUE(maliput::api::test::IsRoadPositionResultClose(expected_road_position_result,
+                                                            dut_->ToRoadPosition(inertial_pos), kLinearTolerance));
+}
+
+TEST_F(RoadGeometryTest, CloseToLaneA) {
+  const maliput::api::InertialPosition inertial_pos{5., 11., 0.};
+  auto lane = dut_->ById().GetLane(kLaneAId);
+
+  const maliput::api::RoadPositionResult expected_road_position_result{
+      {lane, LanePosition{5., 2.5, 0.}},
+      {5., 10., 0.}, /* nearest position */
+      1.,            /* distance */
+  };
+  EXPECT_TRUE(maliput::api::test::IsRoadPositionResultClose(expected_road_position_result,
+                                                            dut_->ToRoadPosition(inertial_pos), kLinearTolerance));
 }
 
 }  // namespace
